71e3c5fcda44414bdebdf017cb5c0a48a4f49cae
rayenwasli <80284684+rayenwasli@users.noreply.github.com>
2025-09-03 14:12:01 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -25,11 +25,11 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Wasli Mohamed Rayen",
+    specialty: "Software Engineer",
     summary:
       "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+    email: "raynwasli@gmail.com",
   },
   experience: [
     {
