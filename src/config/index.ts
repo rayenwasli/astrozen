18f41a9e01cab0c613dc9da0bce636d260708c9b
rayenwasli <80284684+rayenwasli@users.noreply.github.com>
2025-09-03 14:16:41 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -91,7 +91,7 @@ export const SITE_CONTENT: SiteContent = {
 
       Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/rayen-linkedin.jpeg",
   },
 };
 
